e9831453bbc25db08ae7131b50d871701d3a769f
daviddzxy <david.dzxy@gmail.com>
2022-05-29 23:40:33 +0200

    Refactor join message handling

--- internal/chat.go
+++ internal/chat.go
@@ -1,33 +1,10 @@
 package internal
 
 import (
-	"errors"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"sync"
 )
-
-type Room struct {
-	name    string
-	clients map[int]*Client
-}
-
-func NewRoom(name string) *Room {
-	r := &Room{name: name}
-	r.clients = make(map[int]*Client)
-	return r
-}
-
-func (r *Room) getClientDetails() []ClientDetails {
-	users := make([]ClientDetails, len(r.clients))
-	index := 0
-	for _, c := range r.clients {
-		users[index] = ClientDetails{c.clientName, c.id}
-		index += 1
-	}
-	return users
-}
 
 type ClientMessage struct {
 	clientId   int
@@ -35,158 +12,60 @@ type ClientMessage struct {
 }
 
 type Client struct {
-	id         int
-	conn       *websocket.Conn
-	clientName string
-	//TODO
-	//Get rid of clientName, move it to room
+	id           int
+	conn         *websocket.Conn
+	roomSessions map[string]*RoomSession
 }
 
-var idClientGenerator = Generator{}
-var idMessageGenerator = Generator{}
-
-func NewClient(conn *websocket.Conn) *Client {
-	c := &Client{id: idClientGenerator.generateId(), conn: conn, clientName: ""}
+func NewClient(id int, conn *websocket.Conn) *Client {
+	c := &Client{id: id, conn: conn}
+	c.roomSessions = make(map[string]*RoomSession)
 	return c
 }
 
+type RoomSession struct {
+	id     int
+	handle string
+	room   *room
+}
+
+type room struct {
+	handle                 string
+	roomSessions           map[int]*RoomSession
+	roomSessionIdGenerator Generator
+}
+
+func NewChatRoom(handle string) *room {
+	r := &room{handle: handle}
+	r.roomSessions = make(map[int]*RoomSession)
+	return r
+}
+
+func (chatRoom *room) createRoomSession(handle string) *RoomSession {
+	return &RoomSession{id: chatRoom.roomSessionIdGenerator.generateId(), handle: handle, room: chatRoom}
+}
+
 type ChatServer struct {
-	Address       string
-	Pattern       string
-	clients       map[int]*Client
-	clientsByRoom map[int]*Room
-	onConnect     chan *websocket.Conn
-	onClose       chan *ClientMessage
-	onMessage     chan *ClientMessage
-	chatRooms     map[string]*Room
-	upgrader      *websocket.Upgrader
+	Address   string
+	Pattern   string
+	clients   map[int]*Client
+	chatRooms map[string]*room
+	onConnect chan *websocket.Conn
+	onClose   chan *ClientMessage
+	onMessage chan *ClientMessage
+	upgrader  *websocket.Upgrader
 }
 
 func NewChatServer(address string, pattern string) *ChatServer {
 	cs := &ChatServer{Address: address, Pattern: pattern}
+	cs.chatRooms = make(map[string]*room)
 	cs.clients = make(map[int]*Client)
-	cs.clientsByRoom = make(map[int]*Room)
-	cs.chatRooms = make(map[string]*Room)
 	cs.onConnect = make(chan *websocket.Conn)
 	cs.onClose = make(chan *ClientMessage)
 	cs.onMessage = make(chan *ClientMessage)
 	cs.upgrader = &websocket.Upgrader{}
 	cs.upgrader.CheckOrigin = func(request *http.Request) bool { return true } // TODO: implement check origin function
 	return cs
-}
-
-func (cs *ChatServer) Run() {
-	go func() {
-		http.HandleFunc(cs.Pattern, cs.connectionRequestHandler)
-		if err := http.ListenAndServe(cs.Address, nil); err != http.ErrServerClosed {
-			log.Fatalf("Could not start web socket server: %s\n", err)
-		}
-	}()
-	log.Printf("Chat server is listening on %s.\n", cs.Address)
-	for {
-		select {
-		case conn := <-cs.onConnect:
-			c := NewClient(conn)
-			cs.clients[c.id] = c
-			log.Printf("New connection established: %d.\n", c.id)
-			go cs.readFromClient(c)
-		case clientMsg := <-cs.onClose:
-			room := cs.clientsByRoom[clientMsg.clientId]
-			_ = cs.removeClientFromRoom(clientMsg.clientId)
-			cs.broadcastMessage(room, NewRemoveClientMessage(clientMsg.clientId))
-			cs.closeClient(clientMsg.clientId)
-			log.Printf("Connection %d closed.\n", clientMsg.clientId)
-		case clientMsg := <-cs.onMessage:
-			log.Printf("New message %s received from client %d.\n", string(clientMsg.rawMessage), clientMsg.clientId)
-			message, err := ParseClientMessages(clientMsg.rawMessage)
-			if err != nil {
-				log.Printf("Unable to parse client message %s.\n", clientMsg.rawMessage)
-			}
-			client := cs.clients[clientMsg.clientId]
-			switch message.Type {
-			case TextType:
-				textData, _ := message.Data.(Text)
-				cs.handleTextMessage(textData, client)
-			case JoinType:
-				joinData, _ := message.Data.(Join)
-				cs.handleJoinMessage(joinData, client)
-			case PartType:
-				cs.handlePartMessage(client)
-			}
-		}
-	}
-}
-
-func (cs *ChatServer) handleJoinMessage(data Join, c *Client) {
-	clientsDetails := []ClientDetails{}
-	if cs.chatRooms[data.RoomName] != nil {
-		clientsDetails = cs.chatRooms[data.RoomName].getClientDetails()
-	}
-	err := cs.addClientToRoom(c.id, data.RoomName)
-	if err != nil && errors.Is(err, ClientAlreadyInRoomError) {
-		_ = cs.removeClientFromRoom(c.id)
-		_ = cs.addClientToRoom(c.id, data.RoomName)
-	}
-	c.clientName = data.ClientName
-	cs.writeToClient(c, NewSuccessJoinMessage(data.RoomName, clientsDetails))
-	cs.broadcastMessage(cs.chatRooms[data.RoomName], NewAddClientMessage(c.clientName, c.id))
-	log.Printf("Client %d joined room %s with name %s.\n", c.id, data.RoomName, data.ClientName)
-}
-
-func (cs *ChatServer) handlePartMessage(c *Client) {
-	room := cs.clientsByRoom[c.id]
-	if err := cs.removeClientFromRoom(c.id); err == nil {
-		cs.writeToClient(c, NewSuccessPartMessage())
-		cs.broadcastMessage(room, NewRemoveClientMessage(c.id))
-		log.Printf("Client %d left room %s.\n", c.id, room.name)
-	} else {
-		log.Println(err)
-	}
-}
-
-func (cs *ChatServer) removeClientFromRoom(clientId int) error {
-	r := cs.clientsByRoom[clientId]
-	c := cs.clients[clientId]
-	if r != nil {
-		delete(r.clients, c.id)
-		delete(cs.clientsByRoom, c.id)
-		log.Printf("Client %d left room %s.\n", c.id, r.name)
-		if len(r.clients) == 0 {
-			delete(cs.chatRooms, r.name)
-			log.Printf("Room %s was deleted.\n", r.name)
-		}
-		return nil
-	} else {
-		return ClientDoesNotBelongToAnyRoomError
-	}
-}
-
-func (cs *ChatServer) addClientToRoom(clientId int, roomName string) error {
-	c := cs.clients[clientId]
-	r := cs.clientsByRoom[clientId]
-	if r == nil {
-		if cs.chatRooms[roomName] == nil {
-			r = NewRoom(roomName)
-			cs.chatRooms[roomName] = r
-			log.Printf("New room %s has been created.\n", roomName)
-		} else {
-			r = cs.chatRooms[roomName]
-		}
-		r.clients[c.id] = c
-		cs.clientsByRoom[c.id] = r
-		log.Printf("Client %d joined room %s.\n", c.id, roomName)
-		return nil
-	} else {
-		return errors.New("client is already in a room")
-	}
-}
-
-func (cs *ChatServer) handleTextMessage(data Text, c *Client) {
-	room := cs.clientsByRoom[c.id]
-	if room != nil {
-		receiveTextMessage := NewReceiveTextMessage(data.Text, c.id, idMessageGenerator.generateId())
-		cs.broadcastMessage(room, receiveTextMessage)
-	}
 }
 
 func (cs *ChatServer) connectionRequestHandler(responseWriter http.ResponseWriter, request *http.Request) {
@@ -198,7 +77,7 @@ func (cs *ChatServer) connectionRequestHandler(responseWriter http.ResponseWrite
 	cs.onConnect <- conn
 }
 
-func (cs *ChatServer) readFromClient(c *Client) {
+func (cs *ChatServer) readFromConnection(c *Client) {
 	for {
 		_, p, err := c.conn.ReadMessage()
 		msg := &ClientMessage{clientId: c.id, rawMessage: p}
@@ -211,24 +90,11 @@ func (cs *ChatServer) readFromClient(c *Client) {
 	}
 }
 
-func (cs *ChatServer) writeToClient(c *Client, rawMessage []byte) {
-	err := c.conn.WriteMessage(websocket.TextMessage, rawMessage)
-	if err != nil {
-		log.Printf("Unable to send message %s to client %d.\n", string(rawMessage), c.id)
-	}
-	log.Printf("Message %s sent to client %d\n", string(rawMessage), c.id)
-}
-
-func (cs *ChatServer) broadcastMessage(r *Room, rawMessage []byte) {
-	wg := sync.WaitGroup{}
-	wg.Add(len(r.clients))
-	for _, c := range r.clients {
-		go func(c *Client) {
-			defer wg.Done()
-			cs.writeToClient(c, rawMessage)
-		}(c)
-	}
-	wg.Wait()
+func (cs *ChatServer) openClient(id int, conn *websocket.Conn) {
+	c := NewClient(id, conn)
+	cs.clients[c.id] = c
+	go cs.readFromConnection(c)
+	log.Printf("New connection established: %d.\n", c.id)
 }
 
 func (cs *ChatServer) closeClient(id int) {
@@ -237,4 +103,61 @@ func (cs *ChatServer) closeClient(id int) {
 		log.Println(err)
 	}
 	delete(cs.clients, id)
+}
+
+func (cs *ChatServer) Run() {
+	go func() {
+		http.HandleFunc(cs.Pattern, cs.connectionRequestHandler)
+		if err := http.ListenAndServe(cs.Address, nil); err != http.ErrServerClosed {
+			log.Fatalf("Could not start web socket server: %s\n", err)
+		}
+	}()
+	log.Printf("Chat server is listening on %s.\n", cs.Address)
+	clientIdGenerator := Generator{}
+	for {
+		select {
+		case conn := <-cs.onConnect:
+			cs.openClient(clientIdGenerator.generateId(), conn)
+		case clientMsg := <-cs.onClose:
+			cs.closeClient(clientMsg.clientId)
+		case clientMsg := <-cs.onMessage:
+			log.Printf("New message %s received from client %d.\n", string(clientMsg.rawMessage), clientMsg.clientId)
+			msg, err := ParseClientMessages(clientMsg.rawMessage)
+			if err != nil {
+				log.Printf("Unable to parse client message %s.\n", clientMsg.rawMessage)
+			}
+			c := cs.clients[clientMsg.clientId]
+			switch msg.Type {
+			case JoinType:
+				joinData, _ := msg.Data.(Join)
+				cs.handleJoinMessage(joinData, c)
+			}
+		}
+	}
+}
+
+func (cs *ChatServer) handleJoinMessage(msg Join, c *Client) {
+	r := cs.chatRooms[msg.RoomHandle]
+	if r == nil {
+		r = NewChatRoom(msg.RoomHandle)
+	}
+	rs := c.roomSessions[msg.RoomHandle]
+	if rs == nil {
+		rs := &RoomSession{id: r.roomSessionIdGenerator.generateId(), handle: msg.NewRoomSessionHandle, room: r}
+		r.roomSessions[rs.id] = rs
+		c.roomSessions[r.handle] = rs
+		log.Printf("Client %d joined room %s, with room session id %d, handle %s",
+			c.id,
+			r.handle,
+			rs.id,
+			rs.handle,
+		)
+	} else {
+		log.Printf("Client %d already in room %s with room session id %d, handle %s",
+			c.id,
+			r.handle,
+			rs.id,
+			rs.handle,
+		)
+	}
 }
--- internal/messages.go
+++ internal/messages.go
@@ -9,25 +9,14 @@ type Message struct {
 	Data interface{} `json:"data"`
 }
 
-type ClientDetails struct {
-	Name string `json:"name"`
-	Id   int    `json:"id"`
-}
-
 // Messages sent by client
 const (
 	JoinType = "JOIN"
-	PartType = "PART"
-	TextType = "TEXT"
 )
 
 type Join struct {
-	RoomName   string `json:"roomName"`
-	ClientName string `json:"clientName"`
-}
-
-type Text struct {
-	Text string `json:"text"`
+	RoomHandle           string `json:"roomHandle"`
+	NewRoomSessionHandle string `json:"roomSessionHandle"`
 }
 
 func ParseClientMessages(rawMessage []byte) (*Message, error) {
@@ -46,75 +35,6 @@ func ParseClientMessages(rawMessage []byte) (*Message, error) {
 			return nil, err
 		}
 		message.Data = joinData
-	case TextType:
-		var textData Text
-		err := json.Unmarshal(jsonData, &textData)
-		if err != nil {
-			return nil, err
-		}
-		message.Data = textData
 	}
 	return message, nil
-}
-
-// Messages sent by server
-const (
-	SuccessJoinType  string = "SUCCESS_JOIN"
-	SuccessPartType         = "SUCCESS_PART"
-	ReceiveTextType         = "RECEIVE_TEXT"
-	AddClientType           = "ADD_CLIENT"
-	RemoveClientType        = "REMOVE_CLIENT"
-)
-
-type SuccessJoin struct {
-	RoomName      string          `json:"roomName"`
-	ClientDetails []ClientDetails `json:"clients"`
-}
-
-type ReceiveText struct {
-	Text     string `json:"text"`
-	ClientId int    `json:"clientId"`
-	TextId   int    `json:"textId"`
-}
-
-type AddClient struct {
-	ClientDetail ClientDetails `json:"client"`
-}
-
-type RemoveClient struct {
-	Id int `json:"id"`
-}
-
-func NewSuccessJoinMessage(roomName string, users []ClientDetails) []byte {
-	env := &Message{Type: SuccessJoinType}
-	env.Data = &SuccessJoin{roomName, users}
-	jsonMsg, _ := json.Marshal(env)
-	return jsonMsg
-}
-
-func NewSuccessPartMessage() []byte {
-	env := &Message{Type: SuccessPartType}
-	jsonMsg, _ := json.Marshal(env)
-	return jsonMsg
-}
-
-func NewReceiveTextMessage(text string, clientId int, TextId int) []byte {
-	env := &Message{Type: ReceiveTextType}
-	env.Data = &ReceiveText{text, clientId, TextId}
-	jsonMsg, _ := json.Marshal(env)
-	return jsonMsg
-}
-
-func NewAddClientMessage(clientName string, clientId int) []byte {
-	env := &Message{Type: AddClientType}
-	env.Data = &AddClient{ClientDetails{clientName, clientId}}
-	jsonMsg, _ := json.Marshal(env)
-	return jsonMsg
-}
-
-func NewRemoveClientMessage(clientId int) []byte {
-	env := &Message{Type: RemoveClientType}
-	env.Data = &RemoveClient{clientId}
-	jsonMsg, _ := json.Marshal(env)
-	return jsonMsg
 }
